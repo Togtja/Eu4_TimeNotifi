31bcd1aa3cb903d57b87089d5f5bdc9db08b35c5
Tomas Berger <tomashimberg.berger@technipfmc.com>
2021-04-16 14:35:34 +0200

    Add Improved memory reading aftter SO post

--- EU4_C++Time/include/CrossMemory.hpp
+++ EU4_C++Time/include/CrossMemory.hpp
@@ -22,14 +22,30 @@ public:
         return mem->find_byte(find);
     }
 
+    template<class T>
+    std::vector<uint8_t*> find_value(T find) {
+        std::cout << "looking for: " << find << "\n";
+        return mem->find_value(find);
+    }
+
     template<typename T>
     void bytes_to_T(uint8_t addr, T& val) {
         return mem->bytes_to_T(addr, val);
     }
 
+    template<typename T>
+    void bytes_to_T2(uint8_t* addr, T& val) {
+        return mem->bytes_to_T2(addr, val);
+    }
+
     template<class T>
     std::unordered_map<uint8_t, T> find_byte_from_map(T find, const std::unordered_map<uint8_t, T>& map) {
         return mem->find_byte_from_map(find, map);
+    }
+
+    template<class T>
+    std::vector<uint8_t*> find_byte_from_vector(T find, const std::vector<uint8_t*>& vector) {
+        return mem->find_byte_from_vector(find, vector);
     }
     CrossMemory(const std::string& exec_name);
     ~CrossMemory();
--- EU4_C++Time/include/WindowsMem.hpp
+++ EU4_C++Time/include/WindowsMem.hpp
@@ -42,12 +42,11 @@ public:
             if (info.State == MEM_COMMIT && (info.Type == MEM_MAPPED || info.Type == MEM_PRIVATE)) {
                 size_t read{};
                 std::vector<uint8_t> mem_chunk;
-                mem_chunk.resize(info.RegionSize);
-                if (ReadProcessMemory(m_proc, addr, mem_chunk.data(), mem_chunk.size(), &read)) {
-                    mem_chunk.resize(read);
+                mem_chunk.reserve(info.RegionSize);
+                if (ReadProcessMemory(m_proc, addr, mem_chunk.data(), info.RegionSize, &read)) {
                     for (auto pos = mem_chunk.begin();
                          mem_chunk.end() != (pos = std::search(pos, mem_chunk.end(), byte_check.begin(), byte_check.end()));
-                         pos++) {
+                         pos += mem_chunk.size()) {
                         auto lpcvoid       = (addr + (pos - mem_chunk.begin()));
                         auto int_addr      = reinterpret_cast<const char*>(lpcvoid);
                         mapping[*int_addr] = find;
@@ -56,6 +55,57 @@ public:
             }
         }
         return mapping;
+    }
+
+    template<class T>
+    std::vector<uint8_t*> find_value(const T& find) {
+        std::cout << "Searching for value\n";
+
+        std::vector<uint8_t*> found;
+        std::vector<uint8_t> mem_chunk;
+
+        const uint8_t *find_begin, *find_end;
+        size_t find_size;
+
+        if constexpr (std::is_same_v<T, std::string>) {
+            find_begin = reinterpret_cast<const uint8_t*>(find.c_str());
+            find_size  = find.size();
+        }
+        else {
+            find_begin = reinterpret_cast<const uint8_t*>(&find);
+            find_size  = sizeof(find);
+        }
+
+        find_end = find_begin + find_size;
+
+        uint8_t *addr = reinterpret_cast<uint8_t*>(0), *base;
+        MEMORY_BASIC_INFORMATION info;
+
+        while (VirtualQueryEx(m_proc, addr, &info, sizeof(info))) {
+            base = static_cast<uint8_t*>(info.BaseAddress);
+
+            if (info.State == MEM_COMMIT && (info.Type == MEM_MAPPED || info.Type == MEM_PRIVATE)) {
+                mem_chunk.reserve(info.RegionSize);
+
+                size_t read;
+                if (ReadProcessMemory(m_proc, info.BaseAddress, mem_chunk.data(), info.RegionSize, &read)) {
+                    auto start = mem_chunk.data(), end = start + read, pos = start;
+
+                    if (addr > base) {
+                        pos += (addr - base);
+                    }
+
+                    while ((pos = std::search(pos, end, find_begin, find_end)) != end) {
+                        found.push_back(base + (pos - start));
+                        pos += find_size;
+                    }
+                }
+            }
+
+            addr = base + info.RegionSize;
+        }
+
+        return found;
     }
 
     template<class T>
@@ -82,6 +132,30 @@ public:
         return out;
     }
 
+    template<class T>
+    std::vector<uint8_t*> find_byte_from_vector(T find, const std::vector<uint8_t*>& vector) {
+        std::vector<uint8_t*> out;
+        for (const auto& k : vector) {
+            std::vector<uint8_t> byte_check;
+            if constexpr (std::is_same_v<T, std::string>) {
+                byte_check = std::vector<uint8_t>(find.begin(), find.end());
+            }
+            else {
+                uint8_t* data = static_cast<uint8_t*>(static_cast<void*>(&find));
+                byte_check    = std::vector<uint8_t>(data, data + sizeof(find));
+            }
+            std::vector<uint8_t> mem_chunk;
+            mem_chunk.resize(byte_check.size());
+            size_t read;
+            if (ReadProcessMemory(m_proc, k, mem_chunk.data(), mem_chunk.size(), &read)) {
+                if (mem_chunk == byte_check) {
+                    out.push_back(k);
+                }
+            }
+        }
+        return out;
+    }
+
     std::vector<uint8_t> read_address(uint8_t address, size_t size) {
         std::vector<uint8_t> mem_chunk;
         mem_chunk.resize(size);
@@ -101,6 +175,33 @@ public:
         else {
             T res;
             auto bytes   = read_address(addr, sizeof(T));
+            uint8_t* ptr = (uint8_t*)&res;
+            for (int i = 0; i < bytes.size(); i++) {
+                *ptr++ = bytes[i];
+            }
+            val = res;
+        }
+    }
+
+    std::vector<uint8_t> read_address2(uint8_t* address, size_t size) {
+        std::vector<uint8_t> mem_chunk;
+        mem_chunk.resize(size);
+        size_t read;
+        if (ReadProcessMemory(m_proc, address, mem_chunk.data(), mem_chunk.size(), &read)) {
+            return mem_chunk;
+        }
+        return {};
+    }
+
+    template<typename T>
+    void bytes_to_T2(uint8_t* addr, T& val) {
+        if constexpr (std::is_same_v<T, std::string>) {
+            auto bytes = read_address(addr, v.size());
+            val        = std::string(bytes.begin(), bytes.end());
+        }
+        else {
+            T res;
+            auto bytes   = read_address2(addr, sizeof(T));
             uint8_t* ptr = (uint8_t*)&res;
             for (int i = 0; i < bytes.size(); i++) {
                 *ptr++ = bytes[i];
