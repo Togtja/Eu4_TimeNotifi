f6b87e5f3b3c6b6f487feef703c86004d94efe61
Tomas Berger <tomashimberg.berger@technipfmc.com>
2021-05-05 19:04:46 +0200

    Run clang-format and Add pragma once

--- EU4_C++Time/include/NotificationSound.hpp
+++ EU4_C++Time/include/NotificationSound.hpp
@@ -1,42 +1,42 @@
+#pragma once
 #include <AL/al.h>
 #include <AL/alc.h>
 #include <sndfile.h>
 
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
+
 /**
  * I have never really touched OpenAL, so most of this is copypast from:
  * "Code, Tech, and Tutorials": https://www.youtube.com/channel/UC4EJN2OSNdl-mSxGjitRvyA
  * Repo: https://github.com/codetechandtutorials/openal-impl
- * 
+ *
  */
-class NotificationSound
-{
+class NotificationSound {
 private:
-    //Sound Device
+    // Sound Device
     ALCdevice* m_device;
-	ALCcontext* m_context;
+    ALCcontext* m_context;
     const ALCchar* m_device_name = nullptr;
 
-
-    //Sound Playing
+    // Sound Playing
     ALuint m_source;
-	float m_pitch = 1.f;
-	float m_gain = 1.f;
-	float m_position[3] = { 0,0,0 };
-	float m_velocity[3] = { 0,0,0 };
-	bool m_loop_sound = false;
-    ALuint m_buffer = 0;
+    float m_pitch       = 1.f;
+    float m_gain        = 1.f;
+    float m_position[3] = {0, 0, 0};
+    float m_velocity[3] = {0, 0, 0};
+    bool m_loop_sound   = false;
+    ALuint m_buffer     = 0;
+
 public:
     NotificationSound(const std::string& filename);
     ~NotificationSound();
     void play();
     void set_device();
     void set_sound_file(const std::string& filename);
-    std::vector<std::string> get_devices(){
+    std::vector<std::string> get_devices() {
         const ALCchar* devices = alcGetString(m_device, ALC_CAPTURE_DEVICE_SPECIFIER);
         std::cout << devices;
-        
     }
 };
